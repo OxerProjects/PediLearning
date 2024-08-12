e2a2cbf5dec28bf21012e8df45b07d0b9116e569
OxerProjects <122890182+OxerProjects@users.noreply.github.com>
2024-08-13 00:11:57 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('index,ejs', {layout: 'layouts/guestLayout'})
+    res.render('index.ejs', {layout: 'layouts/guestLayout'})
 })
 
 module.exports = router
