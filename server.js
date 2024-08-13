195027b97027239d6df9ed8a2661fefcf0e3ea15
OxerProjects <122890182+OxerProjects@users.noreply.github.com>
2024-08-13 18:07:36 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 //Requires:
 const express = require('express')
 const app = express()
@@ -5,6 +9,13 @@ const expressLayouts = require('express-ejs-layouts')
 const session = require('express-session');
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+
+//DataBase:
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 //Routers:
 const indexRouter = require('./routes/index')
@@ -21,35 +32,33 @@ app.use(methodOverride('_method'))
 app.set('view options', { layout: false });
 app.use(bodyParser.urlencoded({ extended: true }))
 
-//Log In:
-app.use(session({
-  secret: '1997',
-  resave: false,
-  saveUninitialized: true
-}));
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
+//Log In:
 app.get('/login', (req, res) => {
   const error = req.query.error;
   res.render('login', {layout: 'layouts/layout', error})
 })
 
-app.post('/login', (req, res) => {
-  const password = req.body.password;  
-  if (password === "1997") {
-    req.session.authenticated = true;
-    res.redirect('/students');
-  } else {
-    const errorMessage = encodeURIComponent('סיסמה שגויה, נסה שנית...');
-        res.redirect(`/login?error=${errorMessage}`);
-  }
-})
-
-function isAuthenticated(req, res, next) {
-  if (req.session.authenticated) {
-      return next();
-  }
-  res.redirect('/login');
-}
+//app.post('/login', (req, res) => {
+//  const password = req.body.password;  
+//  if (password === "1997") {
+//    req.session.authenticated = true;
+//    res.redirect('/students');
+//  } else {
+//    const errorMessage = encodeURIComponent('סיסמה שגויה, נסה שנית...');
+//        res.redirect(`/login?error=${errorMessage}`);
+//  }
+//})
+//
+//function isAuthenticated(req, res, next) {
+//  if (req.session.authenticated) {
+//      return next();
+//  }
+//  res.redirect('/login');
+//}
 
 // Logout route
 app.get('/logout', (req, res) => {
@@ -59,8 +68,11 @@ app.get('/logout', (req, res) => {
 
 //Routers Config:
 app.use('/', indexRouter)
-app.use('/students', studentsRouter)
-app.use('/cours', systemRouter)
+app.use('/students', isAuthenticated, studentsRouter)
+app.use('/cours', isAuthenticated, systemRouter)
 
 //Hosting
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT, () => {
+  console.log("PORT IS RUNING");
+  
+})
